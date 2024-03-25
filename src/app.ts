38f6c7624fa903d0e5051040ec8a09cd7f343119
Williamima <williamwedeson266@gmail.com>
2024-03-25 17:16:09 -0300

    feat(app.ts): Remoção do Cors

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,11 @@ import "express-async-errors";
 import "dotenv/config";
 import express, { json } from "express";
 import helmet from "helmet";
-import cors from "cors";
 import { carRouter } from "./Routes";
 import { handleError } from "./middlewares";
 
 export const app = express();
 
-app.use(cors())
 
 app.use(helmet());
 app.use(json());
